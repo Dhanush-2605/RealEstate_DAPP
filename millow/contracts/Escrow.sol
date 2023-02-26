0be28187ed0012feb771e0b0520801227b599b39
dhanush <vellasiridhanush@gmail.com>
2023-02-26 22:34:14 +0530

    completed list functionality

--- millow/contracts/Escrow.sol
+++ millow/contracts/Escrow.sol
@@ -14,8 +14,23 @@ contract Escrow {
     address public inspector;
     address payable public seller;
     address public nftAddress;
-mapping(uint256=>bool) isListed;
+
+    modifier onlySeller(){
+        require(msg.sender==seller,"Only seller can call this method");
+        _;
+    }
+    modifier onlyBuyer(uint256 _nftId){
+        require(msg.sender==buyer[_nftId],"Only seller can call this method");
+        _;
+    }
+mapping(uint256=>bool) public isListed;
+
+mapping(uint256=>uint256) public purchasePrice;
+mapping(uint256=>uint256) public escrowAmount;
+mapping(uint256=>address) public buyer;
+
     constructor(
+
         address _nftAddress,
         address payable _seller,
         address _inspector,
@@ -26,10 +41,19 @@ mapping(uint256=>bool) isListed;
         inspector = _inspector;
         seller = _seller;
     }
-    function list(uint256 _nftId) public{
+
+    function list(uint256 _nftId,address _buyer,uint256 _purchasePrice,uint256 _escrowAmount) public payable onlySeller{
         //Transfer nft from seller to this contract
 
         IERC721(nftAddress).transferFrom(msg.sender,address(this),_nftId);
         isListed[_nftId]=true; 
+        purchasePrice[_nftId]=_purchasePrice;
+        escrowAmount[_nftId]=_escrowAmount;
+        buyer[_nftId]=_buyer;
+    }
+
+    //put under contract (only buyer can payable escrow)
+    function depositEarnest(uint256 _nftId) public payable onlyBuyer(_nftId){
+        require(msg.value >= escrowAmount[_nftId]);
     }
 }
--- millow/test/Escrow.js
+++ millow/test/Escrow.js
@@ -38,7 +38,7 @@ describe("Escrow", () => {
     await transaction.wait();
 
     //list property
-    transaction=await escrow.connect(seller).list(1);
+    transaction=await escrow.connect(seller).list(1,buyer.address,tokens(10),tokens(5));
     await transaction.wait();
 
   });
@@ -61,9 +61,27 @@ describe("Escrow", () => {
     });
   });
   describe("Listing", () => {
+    it("Updates as Listed", async () => {
+      // expect(result).to.be.equals(realEstate.address);
+      const result=await escrow.isListed(1);
+      expect(result).to.be.equal(true);
+    });
+
     it("Updates ownership", async () => {
       // expect(result).to.be.equals(realEstate.address);
       expect(await realEstate.ownerOf(1)).to.be.equals(escrow.address);
+    });
+    it("Returns Buyer", async () => {
+      const result=await escrow.buyer(1);
+      expect(result).to.be.equal(buyer.address);
+    });
+    it("Returns Purchase Price", async () => {
+      const result=await escrow.purchasePrice(1);
+      expect(result).to.be.equal(tokens(10));
+    });
+    it("Returns escrow amount", async () => {
+      const result=await escrow.escrowAmount(1);
+      expect(result).to.be.equal(tokens(5));
     });
 
   });
